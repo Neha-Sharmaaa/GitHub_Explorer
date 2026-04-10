78365109440c64e73cb5b2ab22be71cfdde2fc98
Neha-Sharmaaa <neha.k@adypu.edu.in>
2026-04-10 16:47:21 +0530

    refactor: simplify SearchBar UI by removing icons and updating input styling and placeholder text

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,27 +1,18 @@
 import React from 'react';
-import { Search, X } from 'lucide-react';
 
 const SearchBar = ({ query, setQuery, autoFocus }) => {
   return (
-    <div className="search-container">
-      <div className="search-wrapper">
-        <Search className="search-icon-fixed" size={16} />
-        <input
-          type="text"
-          className="search-input"
-          placeholder="Search GitHub users..."
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          autoComplete="off"
-          spellCheck="false"
-          autoFocus={autoFocus}
-        />
-        {query && (
-          <button className="clear-btn" onClick={() => setQuery('')}>
-            <X size={14} />
-          </button>
-        )}
-      </div>
+    <div className="search-input-wrapper">
+      <input
+        type="text"
+        className="search-field"
+        placeholder="Enter codename to scan..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        autoComplete="off"
+        spellCheck="false"
+        autoFocus={autoFocus}
+      />
     </div>
   );
 };
